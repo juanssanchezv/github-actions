16ad69a0c7ced2ef443239bddffa8d84cec064ff
juanssanchezv <js.sanchez130@gmail.com>
2022-03-31 16:09:10 -0500

    first test

--- .github/actions/security/src/main.py
+++ .github/actions/security/src/main.py
@@ -1,20 +1,27 @@
+import shutil
+import os
 from actions_toolkit import core
 
 language = core.get_input('language', required=True)
-core.set_output('output_key', 'output_val')
 
 languages = ['Python', 'JavaScript', 'Kotlin', 'PHP', 'Go', 
               'Shell', 'HCL', 'Swift', 'Typescript', 'C', 
               'Java', 'Vue', 'Ruby', 'Brightscript', 'SCSS', 
               'LookML', 'Groovy', 'CSS', 'HTML', 'PowerShell', 
               'C+', 'C++', 'C#']
-def run(language):
+
+dst = '~/.github/workflows/security.yaml'
+# dst = core.get_input()
+
+def run(language,src, dst):
     try:    
         for i in languages:
             if i==language:
-                # select correct security_<language>.yaml file
-                something = ''
+                os.makedirs('~/.github/workflows/', exist_ok=True)
+                src= str('../security_'+i+'.yaml')
+                shutil.copy2(src, dst)
+
     except Exception as e:
         core.error(f'Error {str(e)}')
-    return something
+
 run
